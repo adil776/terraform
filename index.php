6ca63386c655458393eb1c494767acc970902f49
adil776 <64476003+adil776@users.noreply.github.com>
2020-06-15 13:18:57 +0530

    first

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<h1>ADIL</h1>
+<h3>KING OG BLOGGING</h3>
+
+<p>WELCOME</p>
+<p>THIS IS A TERRAFORM WEBPAGE</p>
+<br>
+<?php
+  $cloudfront_url = `head -n1 mydesti.txt`;
+  $img_path = "https://".$cloudfront_url."/adil.JPG";
+  echo "<br>";
+  echo "<img src='{$img_path}' width=200 height=200>";
+?>
+</body>
+</html>
