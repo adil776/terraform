847ee05f4af8048a0fd35c3cc3aa71fc92d9dd34
adil776 <64476003+adil776@users.noreply.github.com>
2020-06-15 17:47:52 +0530

    new

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <br>
 <?php
   $cloudfront_url = `head -n1 mydesti.txt`;
-  $img_path = "https://".$cloudfront_url."/adil.JPG";
+  $img_path = "C:\Users\Dell\Desktop\terra\task1\hiii\adil.JPG";
   echo "<br>";
   echo "<img src='{$img_path}' width=200 height=200>";
 ?>
