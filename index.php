8849ea5f1e5147ae21e3ecf5bce21749f3e2bfbd
adil776 <64476003+adil776@users.noreply.github.com>
2020-06-16 11:52:05 +0530

    newwwww

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <p>THIS IS A TERRAFORM WEBPAGE</p>
 <br>
 <?php
-  $cloudfront_url = `d3k1qacqoz9wrd.cloudfront.net`;
+  $cloudfront_url = `d1n66u2iu25woi.cloudfront.net`;
   $img_path = "https://".$cloudfront_url."/adil.JPG";
   echo "<br>";
   echo "<img src='{$im_path}' width=600 height=500>";
