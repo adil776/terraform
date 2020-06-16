6d4dfe6f40fce4760898afc4cb2f656b7a6932a6
adil776 <64476003+adil776@users.noreply.github.com>
2020-06-16 13:50:15 +0530

    new4

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
 <body>
+<style>
+background:aqua
 <h1>ADIL</h1>
 
 <p>WELCOME</p>
@@ -8,8 +10,13 @@
 <?php
   $cloudfront_url = `head -n1 my.txt`;
   $img_path = "https://".$cloudfront_url."/img.jpg";
+  $im_path = "https://".aws_instance.myos1.public_ip."/img.jpg";
+  $i_path = "https://".C:/Users/Dell/Desktop/terra/task1."/img.jpg";
   echo "<br>";
-  echo "<img src='{$img_path}' width=300 height=300>";
+  echo "<img src='{$img_path}' width=200 height=200>";
+  echo "<img src='{$im_path}' width=200 height=200>";
+  echo "<img src='{$i_path}' width=200 height=200>";
 ?>
 </body>
 </html>
+</style>
